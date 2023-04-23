c80f7767d62685facad50192caab8e23f2dcdd70
wthoutjc <juancamilorr2012@hotmail.com>
2023-04-23 17:15:22 -0500

    load API's

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,29 +4,29 @@ import { FoodRecents, FoodFeature } from "@/components";
 // Interfaces
 import { IFood, ITable } from "@/interfaces";
 
-async function getRecentFood() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/home/recents-data`
-  );
-  const data = await res.json();
-  console.log("IMPRIMIENDO getRecentFood");
-  console.log(data);
-  return data as IFood[];
-}
+// async function getRecentFood() {
+//   const res = await fetch(
+//     `${process.env.API_URL}/home/recents-data`
+//   );
+//   const data = await res.json();
+//   console.log("IMPRIMIENDO getRecentFood");
+//   console.log(data);
+//   return data as IFood[];
+// }
 
-async function getFeatureFood() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/home/feature-data`
-  );
-  const data = await res.json();
-  console.log("IMPRIMIENDO getFeatureFood");
-  console.log(data);
-  return data as ITable;
-}
+// async function getFeatureFood() {
+//   const res = await fetch(
+//     `${process.env.API_URL}/home/feature-data`
+//   );
+//   const data = await res.json();
+//   console.log("IMPRIMIENDO getFeatureFood");
+//   console.log(data);
+//   return data as ITable;
+// }
 
 const HomePage = async () => {
-  const recentsData = await getRecentFood();
-  const featureData = await getFeatureFood();
+  // const recentsData = await getRecentFood();
+  // const featureData = await getFeatureFood();
 
   return (
     <main
@@ -37,8 +37,8 @@ const HomePage = async () => {
         flexDirection: "column",
       }}
     >
-      <FoodRecents recentsData={recentsData} />
-      <FoodFeature featureData={featureData} />
+      {/* <FoodRecents recentsData={recentsData} />
+      <FoodFeature featureData={featureData} /> */}
     </main>
   );
 };
